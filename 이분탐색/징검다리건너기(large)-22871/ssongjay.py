35135714f7c3bff8e70a7d7b3fb5ad112910baf5
ssongjay <jay5326@naver.com>
2023-04-04 00:48:15 +0900

    initial commit

---
+++ 이분탐색/징검다리건너기(large)-22871/ssongjay.py
@@ -0,0 +1,15 @@
+import sys
+
+input_ = sys.stdin.readline
+
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
